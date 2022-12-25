945d8ded7ab0726a728e3bce4ee095f0161f0f4d
Igor Polunin <garry.818@yandex.ru>
2022-12-25 17:15:16 +0300

    Счетчик на главном экране

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberCount: UILabel!
+    
+    @IBOutlet weak var textCount: UITextField!
+    
+    
+     var count :Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textCount.text = "Значение счетчика: \(count)"
     }
-
-
+    
+    
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        
+        count = count + 1
+        textCount.text = "Значение счетчика: \(count)"
+        print (count)
+        
+    }
+    
 }
+
 
